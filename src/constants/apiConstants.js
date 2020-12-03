cdab61bd9804138ebb29eaddcf10e07614384e26
pranalideogade <pran2871@gmail.com>
2020-12-03 15:40:36 -0500

    api address

--- src/constants/apiConstants.js
+++ src/constants/apiConstants.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = "http://localhost:8080";
-//export const API_BASE_URL = "https://isa-restapi.azurewebsites.net";
+//export const API_BASE_URL = "http://localhost:8080";
+export const API_BASE_URL = "https://isa-restapi.azurewebsites.net";
 export const ACCESS_TOKEN_NAME = 'token';
